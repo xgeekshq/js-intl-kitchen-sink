d22ba08c85046aa5f365b482af32650bfeb874b1
harshanarisetty <harshan010@gmail.com>
2019-10-14 19:12:37 -0400

    404 Component changed to functional approach

--- src/components/404/index.js
+++ src/components/404/index.js
@@ -1,22 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import NotFoundImg from '../../assets/images/404.svg';
 import styles from './styles.module.css';
-export default class NoMatch extends Component {
-  render() {
-    return (
+
+const NoMatch = () => {
+  return (
+    <div>
+      <img src={NotFoundImg} className={styles.image} alt="404"></img>
       <div>
-        <img src={NotFoundImg} className={styles.image} alt="404"></img>
-        <div>
-          <div className={styles.main}>404</div>
-          <div>Sorry, the page you intended is still under development.</div>
-        </div>
-        <div>
-          <Link to="/">
-            <span className="nav-text">Home</span>
-          </Link>
-        </div>
+        <div className={styles.main}>404</div>
+        <div>Sorry, the page you intended is still under development.</div>
       </div>
-    );
-  }
-}
+      <div>
+        <Link to="/">
+          <span className="nav-text">Home</span>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default NoMatch;
