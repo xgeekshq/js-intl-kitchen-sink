3b974a2a464dd3033babaa9550b119e16d3396e8
José Rato <j.rato@xgeeks.io>
2019-10-14 15:59:19 +0100

    feat(DateTimeFormat): Added options to parameters
    #3

--- src/pages/DateTimeFormat/reducer.js
+++ src/pages/DateTimeFormat/reducer.js
@@ -1,4 +1,22 @@
-import { LOCALE_CHANGE, DATE_STYLE_CHANGE } from './constants';
+import {
+  LOCALE_CHANGE,
+  DATE_STYLE_CHANGE,
+  TIME_STYLE_CHANGE,
+  LOCALE_MATCHER_CHANGE,
+  TIME_ZONE_CHANGE,
+  HOUR12_CHANGE,
+  HOUR_CYCLE_CHANGE,
+  FORMAT_MATCHER_CHANGE,
+  WEEK_DAY_CHANGE,
+  ERA_CHANGE,
+  YEAR_CHANGE,
+  MONTH_CHANGE,
+  DAY_CHANGE,
+  HOUR_CHANGE,
+  MINUTE_CHANGE,
+  SECOND_CHANGE,
+  TIME_ZONE_NAME_CHANGE,
+} from './constants';
 
 export const INITIAL_STATE = {
   locale: undefined,
@@ -9,7 +27,7 @@ export const INITIAL_STATE = {
     timeZone: undefined,
     hourCycle: undefined,
     formatMatcher: undefined,
-    weekday: undefined,
+    weekDay: undefined,
     era: undefined,
     year: 'numeric',
     month: 'numeric',
@@ -35,6 +53,126 @@ export default (state = INITIAL_STATE, { type, payload }) => {
         options: {
           ...state.options,
           dateStyle: payload.dateStyle,
+        },
+      };
+    case TIME_STYLE_CHANGE:
+      return {
+        ...state,
+        options: {
+          ...state.options,
+          timeStyle: payload.timeStyle,
+        },
+      };
+    case LOCALE_MATCHER_CHANGE:
+      return {
+        ...state,
+        options: {
+          ...state.options,
+          localeMatcher: payload.localeMatcher,
+        },
+      };
+    case TIME_ZONE_CHANGE:
+      return {
+        ...state,
+        options: {
+          ...state.options,
+          timeZone: payload.timeZone,
+        },
+      };
+    case HOUR12_CHANGE:
+      return {
+        ...state,
+        options: {
+          ...state.options,
+          hour12: payload.hour12,
+        },
+      };
+    case HOUR_CYCLE_CHANGE:
+      return {
+        ...state,
+        options: {
+          ...state.options,
+          hourCycle: payload.hourCycle,
+        },
+      };
+    case FORMAT_MATCHER_CHANGE:
+      return {
+        ...state,
+        options: {
+          ...state.options,
+          formatMatcher: payload.formatMatcher,
+        },
+      };
+    case WEEK_DAY_CHANGE:
+      return {
+        ...state,
+        options: {
+          ...state.options,
+          weekDay: payload.weekDay,
+        },
+      };
+    case ERA_CHANGE:
+      return {
+        ...state,
+        options: {
+          ...state.options,
+          era: payload.era,
+        },
+      };
+    case YEAR_CHANGE:
+      return {
+        ...state,
+        options: {
+          ...state.options,
+          year: payload.year,
+        },
+      };
+    case MONTH_CHANGE:
+      return {
+        ...state,
+        options: {
+          ...state.options,
+          month: payload.month,
+        },
+      };
+    case DAY_CHANGE:
+      return {
+        ...state,
+        options: {
+          ...state.options,
+          day: payload.day,
+        },
+      };
+    case HOUR_CHANGE:
+      return {
+        ...state,
+        options: {
+          ...state.options,
+          hour: payload.hour,
+        },
+      };
+    case MINUTE_CHANGE:
+      return {
+        ...state,
+        options: {
+          ...state.options,
+          minute: payload.minute,
+        },
+      };
+    case SECOND_CHANGE:
+      return {
+        ...state,
+        options: {
+          ...state.options,
+          second: payload.second,
+        },
+      };
+    case TIME_ZONE_NAME_CHANGE:
+      return {
+        ...state,
+        options: {
+          ...state.options,
+          timeZoneName: payload.timeZoneName,
         },
       };
     default:
