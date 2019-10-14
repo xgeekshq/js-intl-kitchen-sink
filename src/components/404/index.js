0b6fc5e038a54182cad925cd98d6ad4e6bb4fd9a
harshanarisetty <harshan010@gmail.com>
2019-10-14 18:23:13 -0400

    404 Component updated to have CSS Modules

--- src/components/404/index.js
+++ src/components/404/index.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import NotFoundImg from '../../assets/images/404.svg';
-import './index.css';
+import styles from './styles.module.css';
 export default class NoMatch extends Component {
   render() {
     return (
-      <div className="container">
-        <img src={NotFoundImg} alt="404"></img>
-        <div className="content">
-          <div className="main">404</div>
-          <div className="sub">
-            Sorry, the page you intended is still under development.
-          </div>
+      <div>
+        <img src={NotFoundImg} className={styles.image} alt="404"></img>
+        <div>
+          <div className={styles.main}>404</div>
+          <div>Sorry, the page you intended is still under development.</div>
         </div>
         <div>
           <Link to="/">
