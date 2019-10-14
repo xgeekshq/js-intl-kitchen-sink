c4193b64f7771d965d4f8ba2efe6b8f0510c7876
harshanarisetty <harshan010@gmail.com>
2019-10-14 17:43:08 -0400

    404 Page And Routes Funtionality implemented

--- src/App.js
+++ src/App.js
@@ -1,57 +1,39 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Result } from 'antd';
-
 import GA from './utils/GoogleAnalytics';
 import PageLayout from './components/Layout';
 
 import Home from './pages/Home';
 import DateTimeFormat from './pages/DateTimeFormat';
 import RelativeTimeFormat from './pages/RelativeTimeFormat';
-import NoMatch from './pages/404';
-// Temp
-const notImplemented = () => (
-  <Result
-    status="404"
-    title="404"
-    subTitle="Sorry, the page you intended is still under development."
-    extra={
-      <Link to="/">
-        <span className="nav-text">Home</span>
-      </Link>
-    }
-  />
-);
+import NoMatch from './components/404';
 
 function App() {
   return (
     <Router>
       {GA.init() && <GA.RouteTracker />}
       <PageLayout>
-        <Route exact path="/" component={Home} />
-        <Route
-          exact
-          path="/DateTimeFormat"
-          component={
-            process.env.NODE_ENV === 'production'
-              ? notImplemented
-              : DateTimeFormat
-          }
-        />
-        <Route exact path="/NumberFormat" component={notImplemented} />
-        <Route exact path="/Collator" component={notImplemented} />
-        <Route exact path="/PluralRules" component={notImplemented} />
-        <Route
-          exact
-          path="/RelativeTimeFormat"
-          component={
-            process.env.NODE_ENV === 'production'
-              ? notImplemented
-              : RelativeTimeFormat
-          }
-        />
-        <Route exact path="/getCanonicalLocales" component={notImplemented} />
-        <Route component={NoMatch} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route
+            exact
+            path="/DateTimeFormat"
+            component={
+              process.env.NODE_ENV === 'production' ? NoMatch : DateTimeFormat
+            }
+          />
+          <Route
+            exact
+            path="/RelativeTimeFormat"
+            component={
+              process.env.NODE_ENV === 'production'
+                ? NoMatch
+                : RelativeTimeFormat
+            }
+          />
+          <Route component={NoMatch} />
+        </Switch>
       </PageLayout>
     </Router>
   );
