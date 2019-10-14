a0c3eee4e8e50fa8008d8d96fe610666599573b6
Lawrence <Lawrence4code@gmail.com>
2019-10-14 23:52:16 +0530

    Add twitter share icons in home page

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Layout, PageHeader } from 'antd';
 import styles from './styles.module.css';
+import { Icon } from 'antd';
 
 const { Header: HeaderAntd } = Layout;
 
@@ -8,6 +9,18 @@ const Header = () => {
   return (
     <HeaderAntd className={styles.header}>
       <PageHeader title="JS Intl Kitchen Sink" subTitle="Home" />
+      <a href="https://twitter.com/xgeeksio" className={styles.twitterFollow}>
+        <Icon
+          type="twitter"
+          href="https://twitter.com/xgeeksio"
+          style={{
+            padding: '10px',
+            fontSize: '1.5rem',
+            color: '#fff',
+            cursor: 'pointer',
+          }}
+        />
+      </a>
     </HeaderAntd>
   );
 };
