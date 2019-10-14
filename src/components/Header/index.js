2c6aa9535caa0319ac68adf0a2e94c1fe6a07e9b
Lawrence <Lawrence4code@gmail.com>
2019-10-15 00:32:47 +0530

    Add missing target and rel attributes in a tag

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,7 +9,12 @@ const Header = () => {
   return (
     <HeaderAntd className={styles.header}>
       <PageHeader title="JS Intl Kitchen Sink" subTitle="Home" />
-      <a href="https://twitter.com/xgeeksio" className={styles.twitterFollow}>
+      <a
+        href="https://twitter.com/xgeeksio"
+        rel="noopener noreferrer"
+        target="_black"
+        className={styles.twitterFollow}
+      >
         <Icon
           type="twitter"
           href="https://twitter.com/xgeeksio"
