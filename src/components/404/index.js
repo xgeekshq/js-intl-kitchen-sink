a0164ec2006786721935205dea0ea786e0a109d3
harshanarisetty <harshan010@gmail.com>
2019-10-14 19:16:50 -0400

    404 Page Added and Routes Functionality updated (#32)
    * 404 Page added
    
    * 404 Page And Routes Funtionality implemented
    
    * 404 Component updated to have CSS Modules
    
    * 404 Component changed to functional approach

---
+++ src/components/404/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import NotFoundImg from '../../assets/images/404.svg';
+import styles from './styles.module.css';
+
+const NoMatch = () => {
+  return (
+    <div>
+      <img src={NotFoundImg} className={styles.image} alt="404"></img>
+      <div>
+        <div className={styles.main}>404</div>
+        <div>Sorry, the page you intended is still under development.</div>
+      </div>
+      <div>
+        <Link to="/">
+          <span className="nav-text">Home</span>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default NoMatch;
