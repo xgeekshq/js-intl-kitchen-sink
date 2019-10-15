f7f6e6c84eb06757345303bd7c3dfca326823f11
Luis Oliveira (xGeeks) <l.oliveira@xgeeks.io>
2019-10-15 14:33:48 +0100

    fix(mainmenu): on back to home from NotFound update selected sidebar option

--- src/components/MainMenu/index.js
+++ src/components/MainMenu/index.js
@@ -1,42 +1,50 @@
 import React from 'react';
 import { Menu, Icon } from 'antd';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const MainMenu = () => {
+  const location = useLocation();
+
+  console.log(location);
   return (
-    <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-      <Menu.Item key="1">
+    <Menu
+      theme="dark"
+      mode="inline"
+      defaultSelectedKeys={['1']}
+      selectedKeys={[location.pathname]}
+    >
+      <Menu.Item key="/">
         <Link to="/">
           <Icon type="home" />
           <span className="nav-text">Home</span>
         </Link>
       </Menu.Item>
-      <Menu.Item key="2">
+      <Menu.Item key="/DateTimeFormat">
         <Link to="/DateTimeFormat">
           <span className="nav-text">DateTimeFormat</span>
         </Link>
       </Menu.Item>
-      <Menu.Item key="3">
+      <Menu.Item key="/NumberFormat">
         <Link to="/NumberFormat">
           <span className="nav-text">NumberFormat</span>
         </Link>
       </Menu.Item>
-      <Menu.Item key="4">
+      <Menu.Item key="/Collator">
         <Link to="/Collator">
           <span className="nav-text">Collator</span>
         </Link>
       </Menu.Item>
-      <Menu.Item key="5">
+      <Menu.Item key="/PluralRules">
         <Link to="/PluralRules">
           <span className="nav-text">PluralRules</span>
         </Link>
       </Menu.Item>
-      <Menu.Item key="6">
+      <Menu.Item key="/RelativeTimeFormat">
         <Link to="/RelativeTimeFormat">
           <span className="nav-text">RelativeTimeFormat</span>
         </Link>
       </Menu.Item>
-      <Menu.Item key="7">
+      <Menu.Item key="/getCanonicalLocales">
         <Link to="/getCanonicalLocales">
           <span className="nav-text">getCanonicalLocales</span>
         </Link>
