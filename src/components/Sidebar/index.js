071d6690b9cd51687e7bf6df74519e344c6aed8a
Luis Oliveira (xGeeks) <l.oliveira@xgeeks.io>
2019-10-16 07:11:12 +0100

    refactor(sidebar): remove commented code

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -36,25 +36,6 @@ const Sidebar = () => {
           <Ads />
         </div>
       </div>
-      {/* <div className={styles.logo}>
-        <img
-          src="https://www.xgeeks.io/assets/xgeeks_logo_white.svg"
-          alt="logo"
-        />
-        <p>Open Source</p>
-      </div>
-      <MainMenu />
-      <br />
-      <Card style={{ margin: 10 }} bodyStyle={{ padding: 0 }}>
-        <div style={{ padding: 10 }}>
-          <h4>Something not working?</h4>
-          <a href="https://github.com/xgeekshq/js-intl-kitchen-sink/issues/new?assignees=&labels=bug&template=bug_report.md&title=%5BBUG%5D%3A+">
-            Let us know
-          </a>
-        </div>
-      </Card>
-      <br />
-      <Ads /> */}
     </SiderAntd>
   );
 };
