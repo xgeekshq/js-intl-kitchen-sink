3034f02073349639aecee753b45596a53b298edb
Pitchayut Chitsinpchayakun <pitchayut@oxygenai.com>
2019-10-16 09:58:02 +0700

    change sidebar to flexbox to fix issue with position relative on device with large height

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -11,15 +11,26 @@ const { Sider: SiderAntd } = Layout;
 const Sidebar = () => {
   return (
     <SiderAntd className={styles.sider}>
-      <div className={styles.logo}>
-        <img
-          src="http://www.xgeeks.io/assets/xgeeks_logo_white.svg"
-          alt="logo"
-        />
-        <p>Open Source</p>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          flexDirection: 'column',
+          height: '100%',
+        }}
+      >
+        <div>
+          <div className={styles.logo}>
+            <img
+              src="http://www.xgeeks.io/assets/xgeeks_logo_white.svg"
+              alt="logo"
+            />
+            <p>Open Source</p>
+          </div>
+          <MainMenu />
+        </div>
+        <Ads />
       </div>
-      <MainMenu />
-      <Ads />
     </SiderAntd>
   );
 };
