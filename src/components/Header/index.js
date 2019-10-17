80b62c5e522614f640587718e32cf0288f5d0af9
Rajshri Singh Baghel <rsbaghel@thoughtworks.com>
2019-10-17 21:51:14 +0530

    Update SubTitle in main page header (#77)

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Layout, PageHeader } from 'antd';
 import styles from './styles.module.css';
+import { useLocation } from 'react-router-dom';
 import { Icon } from 'antd';
 
 const { Header: HeaderAntd } = Layout;
 
 const Header = () => {
+  const pathname = useLocation().pathname;
+  const subTitle = pathname == '/' ? 'Home' : pathname.split('/')[1];
   return (
     <HeaderAntd className={styles.header}>
-      <PageHeader title="JS Intl Kitchen Sink" subTitle="Home" />
+      <PageHeader title="JS Intl Kitchen Sink" subTitle={subTitle} />
       <a
         href="https://twitter.com/xgeeksio"
         rel="noopener noreferrer"
