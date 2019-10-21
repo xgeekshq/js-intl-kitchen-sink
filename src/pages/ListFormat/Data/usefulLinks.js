7dabffd9211dfd48199feb9689aa4d3209d7831b
Cesar Boaventura <cesar.boaventura61@gmail.com>
2019-10-20 22:05:50 -0300

    feat: update footer links

--- src/pages/ListFormat/Data/usefulLinks.js
+++ src/pages/ListFormat/Data/usefulLinks.js
@@ -2,23 +2,23 @@ const links = [
   {
     title: 'MDN web docs Mozilla',
     text:
-      'The Intl.RelativeTimeFormat object is a constructor for objects that enable language-sensitive relative time formatting.',
+      'The Intl.ListFormat object is a constructor for objects that enable language-sensitive list formatting.',
     avatar:
       'https://developer.mozilla.org/static/img/favicon32.7f3da72dcea1.png',
     link:
-      'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RelativeTimeFormat',
+      'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/ListFormat',
   },
   {
     title: 'v8.dev',
-    text: 'Intl.RelativeTimeFormat',
+    text: 'Intl.ListFormat',
     avatar: 'https://v8.dev/favicon.ico',
-    link: 'https://v8.dev/features/intl-relativetimeformat',
+    link: 'https://v8.dev/features/intl-listformat',
   },
   {
     title: 'TC39',
-    text: 'Intl.RelativeTimeFormat Spec Proposal',
+    text: 'Intl.ListFormat',
     avatar: 'https://tc39.es/favicon.ico',
-    link: 'https://tc39.es/proposal-intl-relative-time/',
+    link: 'https://tc39.es/proposal-intl-list-format/',
   },
 ];
 
