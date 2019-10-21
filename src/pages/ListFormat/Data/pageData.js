074f3c0d845cfa441ba5a7fb4ac80803e9352380
Cesar Boaventura <cesar.boaventura61@gmail.com>
2019-10-20 22:07:14 -0300

    feat: update pageData

--- src/pages/ListFormat/Data/pageData.js
+++ src/pages/ListFormat/Data/pageData.js
@@ -1,10 +1,7 @@
-export const units = [
-  'year',
-  'quarter',
-  'month',
-  'week',
-  'day',
-  'hour',
-  'minute',
-  'second',
+export const initialListArray = ['Motorcycle', 'Bus', 'Car'];
+
+export const initialListValue = [
+  { id: 1, value: 'Motorcycle' },
+  { id: 2, value: 'Bus' },
+  { id: 3, value: 'Car' },
 ];
