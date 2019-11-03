816c21703c31426c93a3d9ea8c54a630ca8de504
fllprbt <pgiouvanelis@gmail.com>
2019-11-03 23:49:58 +0100

    Refinement in popoverContent

--- src/components/PopoverContent/index.js
+++ src/components/PopoverContent/index.js
@@ -4,8 +4,12 @@ import styles from './styles.module.css';
 
 import QuestionMarkImg from '../../assets/images/question.svg';
 
-const PopoverContent = ({ children, ...other }) => (
-  <div className={styles.container} {...other}>
+const PopoverContent = ({ children, onMouseEnter, onMouseLeave }) => (
+  <div
+    className={styles.container}
+    onMouseEnter={onMouseEnter}
+    onMouseLeave={onMouseLeave}
+  >
     {children}
     <img alt="" className={styles.image} src={QuestionMarkImg} />
   </div>
